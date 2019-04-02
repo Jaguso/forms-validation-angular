b2541d7d5933824ca5a60a5c7fd5c81832d8b247
Jaguso <jagutzo@gmail.com>
2019-04-02 14:43:11 -0600

    configured router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule, Routes, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
     FormsModule
   ],
